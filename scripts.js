2071c25d3494f854149aa3c5f25628018cb74fd0
Jonathan White <jonathan.white.jm@gmail.com>
2020-11-01 21:11:31 -0500

    Updated js, Updated css, update html w/ images

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+    var btn = document.querySelector("button");
+    var textMsg = docuement.getElementByClassName("message");
+
+    btn.addEventListener("click", function(i){
+        i.preventDefault();
+        var email = document.getElementById("email").value;
+        if (document.getElementById("email").value = ""){
+            textMsg.innerHTML = "Thank you! Your email address" + email + "has been added to our mailing list!";
+        } else {
+            textMsg.innerHTML = "Please enter a valid email address";
+        }
+
+
+    });
+
+
+
+
+}
